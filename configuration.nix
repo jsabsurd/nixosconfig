084e615c705ad657697f1a60acb2c0bc2fc97ebf
johnny <joh.stauch@proton.me>
2024-11-27 12:50:33 +0100

    gnome dazu

--- configuration.nix
+++ configuration.nix
@@ -119,6 +119,29 @@ fonts.packages = with pkgs; [
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
+services.xserver = {
+  enable = true;
+  displayManager.gdm.enable = true;
+  desktopManager.gnome.enable = true;
+};
+environment.gnome.excludePackages = (with pkgs; [
+  gnome-photos
+  gnome-tour
+  gedit # text editor
+]) ++ (with pkgs.gnome; [
+  cheese # webcam tool
+  gnome-music
+  gnome-terminal
+  epiphany # web browser
+  geary # email reader
+  evince # document viewer
+  gnome-characters
+  totem # video player
+  tali # poker game
+  iagno # go game
+  hitori # sudoku game
+  atomix # puzzle game
+]);
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
